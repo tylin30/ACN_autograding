9b63f5267629eb4477d1773211988385341d058c
Ting-Yu Lin <36153134+tylin30@users.noreply.github.com>
2019-03-08 18:48:04 +0800

    doc -> pdf

--- ACN_HM_workflow.sh
+++ ACN_HM_workflow.sh
@@ -1,6 +1,10 @@
 cd ${1-./}
 
-# rename file to studentID.docx/.pdf
+#conver .docx to .pdf
+unoconv *.docx 
+mkdir -p pdf
+
+# rename file to studentID.pdf
 rename 's/^hw\d{6}_([a-z].{8}|es_\d{5})_[^_]*_(\d{1,2})/$1_$2/' *.pdf
 
 
@@ -8,13 +12,18 @@ rename 's/^hw\d{6}_([a-z].{8}|es_\d{5})_[^_]*_(\d{1,2})/$1_$2/' *.pdf
 for file in *.pdf;
 do
 	# filename get student ID
-	# studentID=$(echo $file | sed -e 's/\..*//')
 	studentID=$(echo $file | sed -e 's/_.\..*//')
+	
+	#get file version from same student
 	# tmp=$(echo $file | sed -e 's/\..*//')
 	# version=$(echo $tmp | sed -e 's/.*\([0-9]$\)/\1/')
-	filenumber=$(ls -dq *$studentID* | wc -l)
+	# version=$(echo $version | sed -e 's/ //')
 	
-	#if there are more than two files from same person
+	#get the latest version from same student
+	filenumber=$(ls -dq *$studentID* | wc -l)
+	# filenumber=$(echo $filenumber | sed -e 's/ //')
+	
+	#if there are more than two files from same student
 	#remove previous version
 	if [ $filenumber -ge 2 ]; then
 		for ((i=1; i<$filenumber; i++));
@@ -22,11 +31,20 @@ do
 			rm echo $studentID"_"$i".pdf"
 			# echo $studentID"_"$i".pdf"
 		done
+
 	fi
+	# if [ "$version" -eq "$filenumber" ]; then
+	# 	#move finished pdf to pdf folder
+	# 	mv $studentID"_"$filenumber".pdf" ./pdf;
+	# 	# echo "version==filenumber"
+	# fi
+	
+	
 done
 
+
 #insert data colnames
-echo studemtID, pages, lines, words, chars, >> result.csv
+echo studentID, pages, lines, words, chars, >> result.csv
 
 #count lines, words, chars, filename, studentID
 for file in *.pdf;
@@ -45,6 +63,13 @@ do
 	pages=$(mdls -name kMDItemNumberOfPages -raw $file)
 
 	#output
+
 	echo $studentID,$pages,$lines,$words,$chars >> result.csv;
+	mv $file ./pdf;
 	# echo $filenumber >> result.csv;
 done
+
+#move all .docx to docx folder
+mkdir -p docx
+mv *.docx ./docx
+
